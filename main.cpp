f8d49d98b856a0d4be4cbbaa67af4876ff6a2259
MCLEANS <jackagwaya@gmail.com>
2020-12-10 12:24:36 +0300

    Initialzed sensor

--- main.cpp
+++ main.cpp
@@ -26,6 +26,8 @@
 #define OUT_Z_L 0X2D
 #define OUT_Z_H 0X2D
 
+#define LIS3DH_ID 63
+
 int16_t mydata_x = 20;
 int16_t mydata_y = 20;
 int16_t mydata_z = 20;
@@ -73,75 +75,9 @@ void reset_cs_pin(){
   CS_PORT->ODR &= ~(1 << CS_PIN);
 }
 
-uint16_t get_device_ID(){
-  uint16_t device_ID;
-  reset_cs_pin();
-  //Read from the WHO_AM_I register
-  device_ID = motion_sensor.read(((0x80 | WHO_AM_I) << 8));
-  set_cs_pin();
-  //clean up the received ID by removing the higher order 8 bits
-  device_ID &= ~(0xFF << 8);
-
-  return device_ID;
-}
-
-void initialize(){
-  /**
-   * 1.Set Control register 1 (20h) to High resolution Mode
-   * 2.Enable X,Y and Z axis
-   * 3.Configure data rate to 100Hz
-   */
-  reset_cs_pin();
-  motion_sensor.write((20 << 8)| 0x5F);
-  set_cs_pin();
-
-  /**
-   * 1. Enable High resolution mode in Control register 4 (23h)
-   * 2. Set full scale selection to default (+-2g)
-   */
-  reset_cs_pin();
-  motion_sensor.write((23 << 8)| 0x08);
-  set_cs_pin();
-
-  /**
-   * 1. Reboot memory content in control register 5(24h)
-  
-  reset_cs_pin();
-  motion_sensor.write((CTRL_REG5 << 8)| 0x80);
-  set_cs_pin();
-
-  reset_cs_pin();
-  motion_sensor.write((CTRL_REG5 << 8)| 0x00);
-  set_cs_pin();
- */
-}
-
-void read_accel_values(){
-  //Read status register
-  reset_cs_pin();
-  uint16_t status_register = motion_sensor.read(((0x80 | 0x0F) << 8));
-  set_cs_pin();
-  status_register &= ~(0xFF << 8);
-  //check if new value is available
-  if(status_register & (1<<3)){
-  //  if(status_register & (1<<7)){
-      //Read X-OUT LOW AND HIGH
-      reset_cs_pin();
-      uint16_t x_out_low = motion_sensor.read(((0x80 | OUT_X_L) << 8));
-      set_cs_pin();
-      x_out_low &= ~(0xFF << 8);
-      uint8_t x_axis_low = x_out_low;
-     
-   }
-  //}
-
-
-}
-
 int main(void) {
   
   system_clock.initialize();
-  NOKIA.inverted_mode();
 
   //Initialize CHIP SELECT PIN
   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOEEN;
@@ -150,23 +86,37 @@ int main(void) {
   //SET TO VERY HIGH SPEED
   CS_PORT->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR3;
   set_cs_pin();
-
   
+  //Initialize the sensor
 
+  /**
+   * Configure Control register 0
+   * 1. Initialize the register as indicated in datasheet (This is needed for the sensor to function optimally)
+   */
+
+  reset_cs_pin();
+  motion_sensor.write((0x1E << 8)| 0x10);
+  set_cs_pin();
+
+  /**
+   * configure control register 1
+   * 1. Set Normal mode. 
+   * 2. Set 100Hz sampling rate
+   */
   reset_cs_pin();
   motion_sensor.write((0x20 << 8)| 0x57);
   set_cs_pin();
 
+  /**
+   * Configure Control Register 4
+   * 1. Clear HR bit to set Normal Mode
+   * 2. Spi Interface mode selection (Set to 4-wire).
+   * 3. Full scale selected to +-2g
+   */
+
   reset_cs_pin();
   motion_sensor.write((0x23 << 8)| 0x00);
   set_cs_pin();
- 
-  
-
-
-  
- // initialize();
-
 
   //Read from the WHO_AM_I register
   reset_cs_pin();
@@ -175,7 +125,6 @@ int main(void) {
   mydata_whoami &=  ~(0xFF << 8);
 
   while(1){
-
 
   /**
    *  read Raw values for the Y-AXIS
